3931e9c142bd3b0b838db0e5f1f6df6cc827b7a1
harsh-quare <harshdadhich2019@gmail.com>
2025-09-22 20:49:49 +0530

    fixed typo in spinner

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 function Spinner() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <Loader2 className="w-8 h-8 animate-spin focus:ring-indigo-500" />
+      <Loader2 className="w-15 h-15 animate-spin text-indigo-600" />
     </div>
   );
 }
