62f1341c5e0760a166e6de904a8fdeb17eea5b5b
harsh-quare <harshdadhich2019@gmail.com>
2025-09-22 20:44:49 +0530

    fixed spinner route

--- client/src/components/GuestRoute.jsx
+++ client/src/components/GuestRoute.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import Spinner from './Spinner';
 
 function GuestRoute() {
   const { isAuthenticated, loading } = useAuth();
