66c12eaabdd03cf76ad7d4459f63e2bf80525144
harsh-quare <harshdadhich2019@gmail.com>
2025-09-23 22:54:37 +0530

    loader added

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import Spinner from './Spinner';
 
 function ProtectedRoute() {
   const { isAuthenticated, loading } = useAuth();
 
   // Show a loader while authentication state is being checked
   if (loading) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 
   // Redirect to login if the user is not authenticated
